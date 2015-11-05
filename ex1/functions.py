9a819eddc00990eb7ed73dcc46ad7647e0faa729
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2015-11-05 16:37:24 +0100

    More examples for dictionaries and lists

--- ex1/functions.py
+++ ex1/functions.py
@@ -41,6 +41,10 @@ def f():
     """
     return "success"
 
+##############################
+# Basic function definitions #
+##############################
+
 # Define a function named add that takes two numbers and returns the sum.
 
 # Define a function named to_tuple that takes three arguments and returns a
@@ -51,6 +55,10 @@ def f():
 
 # Define a function named check_n that check is a number is greater than n. The
 # number should be the first argument and n the second
+
+#########
+# LISTS #
+#########
 
 # Define a function named check_list that takes two arguments. The first
 # argument is a list of numbers and the second argument is the number n to
@@ -70,3 +78,38 @@ def f():
 # element (including the first one). Use a keyword named nth to set the default
 # value for nth to 2.
 
+# Define a function named search_n that takes a list and a variable x and
+# searches for x in the list. If the variable is found return the index of the
+# variable in the list and the variable. Otherwise use None for both return
+# values
+
+################
+# Dictionaries #
+################
+
+# Define a function named args_to_dict that takes three arguments and returns a
+# dictionary with the position of the argument as the key (starting at 0) and
+# the argument as the value.
+
+# BONUS: Write a function named args_to_dict_general that does the same for any
+# number of arguments
+
+# Define a function named lists_to_dict that takes two lists of equal lenght
+# named keys and values and builds a dictionary out of them.
+
+# Define a function named search_list that takes two lists a and b. The
+# function searches for all elements of b in list a. The return value should be
+# a dictionary containing the index of the found element of b in list a and the
+# value of the found element. If nothing was found then return an empty
+# dictionary.
+
+# Define a function named dict_to_string that takes a dictionary and a
+# separator string. The function should only take elements out of the
+# dictionary whose value is a string and then return a single string containing
+# the strings stored in the dictionary seperated by the separator string.
+# Return an empty string if there are no strings in the dictionary.
+
+# Define a function named classify_by_type which takes a list l and returns a
+# dictionary d. The d must have the keys 'int' and 'str' which contain the
+# elements out of l that have this type. Elements that do not fit one of these
+# two types should be stored in a list under the key 'others'
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -93,3 +93,83 @@ def test_remove_nth():
     assert func.remove_nth(l) == func.remove_nth(l, 2)
     assert func.remove_nth(l, 1) == []
     assert func.remove_nth(l, nth=3) == [3, 4, 34, 67, -4]
+
+
+def test_search_n():
+    l = ['test', 4, 2.8, 'text']
+    x = 2.8
+    x1 = 23
+    x2 = 'test'
+    assert func.search_n(l, x) == (2, x)
+    assert func.search_n(l, x1) == (None, None)
+    assert func.search_n(l, x2) == (0, x2)
+
+
+def test_args_to_dict():
+    assert func.args_to_dict('1', 2, 'Mango') == {0: '1',
+                                                  1: 2,
+                                                  2: 'Mango'}
+
+
+def test_args_to_dict_general():
+    assert func.args_to_dict_general('1', 2, 'Mango') == {0: '1',
+                                                          1: 2,
+                                                          2: 'Mango'}
+    assert func.args_to_dict_general('1', 'Mango') == {0: '1',
+                                                       1: 'Mango'}
+    l = [23, 'Banana', 4, 'Jan', 'Feb']
+    assert func.args_to_dict_general(*l) == {0: 23,
+                                             1: 'Banana',
+                                             2: 4,
+                                             3: 'Jan',
+                                             4: 'Feb'}
+
+
+def test_lists_to_dict():
+    keys = [23, 'Banana', 4, 'Jan', 'Feb']
+    values = [0, 'yummy', 2, 1, 2]
+    assert func.lists_to_dict(keys, values) == {23: 0,
+                                                'Banana': 'yummy',
+                                                4: 2,
+                                                'Jan': 1,
+                                                'Feb': 2}
+    assert func.lists_to_dict([], []) == {}
+
+
+def test_search_list():
+    l = ['test', 4, 2.8, 'text']
+    ls = [2.8, 2, 5, 'test']
+    assert func.search_list(l, ls) == {2: 2.8,
+                                       0: 'test'}
+    assert func.search_list(l, [45, 22, 'nono']) == {}
+    assert func.search_list(l, []) == {}
+
+
+def test_dict_to_string():
+    d = {1: 'test',
+         2: 4,
+         3: 2.8,
+         '5': 'text'}
+    assert func.dict_to_string(d, ':') == 'test:text'
+    d2 = {'one': 1,
+          'two': 2,
+          'three': 3}
+    assert func.dict_to_string(d2, '-') == ''
+    d3 = {1: 'one',
+          2: 'two',
+          3: 'three'}
+    assert func.dict_to_string(d3, '-') == 'one-two-three'
+
+
+def test_classify_by_type():
+    l = [23, 'Banana', 4, 'Jan', 'Feb', 8.3]
+    assert func.classify_by_type(l) == {'int': [23, 4],
+                                        'str': ['Banana', 'Jan', 'Feb'],
+                                        'others': [8.3]}
+    l = [23, 'Banana', 4, 'Jan', 'Feb']
+    assert func.classify_by_type(l) == {'int': [23, 4],
+                                        'str': ['Banana', 'Jan', 'Feb'],
+                                        'others': []}
+    assert func.classify_by_type([]) == {'int': [],
+                                         'str': [],
+                                         'others': []}
